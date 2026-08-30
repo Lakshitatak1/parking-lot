428209652cb4471dbbaaf48540c54f82fd940cb9
Lakshitatak1 <92687061+Lakshitatak1@users.noreply.github.com>
2026-08-30 20:59:39 +0530

    Create ParkingSpot.java

---
+++ src/main/java/com/parkinglot/model/ParkingSpot.java
@@ -0,0 +1,43 @@
+package com.parkinglot.model;
+
+import com.parkinglot.enums.SpotType;
+
+public class ParkingSpot {
+    private final String spotId;
+    private final SpotType spotType;
+    private Vehicle vehicle;
+
+    public ParkingSpot(String spotId, SpotType spotType) {
+        this.spotId = spotId;
+        this.spotType = spotType;
+    }
+
+    public String getSpotId() {
+        return spotId;
+    }
+
+    public SpotType getSpotType() {
+        return spotType;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void parkVehicle(Vehicle vehicle) {
+        if(this.vehicle != null) {
+            throw new IllegalStateException("Parking spot is already occupied.");
+        }
+        if(vehicle == null) {
+            throw new IllegalArgumentException("Vehicle cannot be null.");
+        }
+        this.vehicle = vehicle;
+    }
+
+    public void removeVehicle() {
+        if(this.vehicle == null) {
+            throw new IllegalStateException("Parking spot is already empty.");
+        }
+        this.vehicle = null;
+    }
+}
